73ccf0431e29f4e4d632775676066eff73a2b15d
Christopher Allen <ChristopherA@hackathon.net>
2015-04-24 16:22:38 -0700

    commented out keepalive

--- prepare-osx-for-webdev.sh
+++ prepare-osx-for-webdev.sh
@@ -48,7 +48,7 @@ if [[ `uname` == 'Darwin' ]]; then
     sudo -v
 
     # Keep-alive: update existing `sudo` time stamp until script has finished
-    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+    # while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
     # Define some variables...
     tmp_file=".softwareupdate.$$"
