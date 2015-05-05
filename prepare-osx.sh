7f1eec04089b9214f63270a55c1d070800f01582
Christopher Allen <ChristopherA@hackathon.net>
2015-05-05 01:18:01 -0700

    Shorten name of script file

--- prepare-osx.sh
+++ prepare-osx.sh
@@ -9,7 +9,7 @@
 
 # Execute on a new machine via:
 
-# $ curl -L https://raw.githubusercontent.com/ChristopherA/prepare-osx-for-webdev/master/prepare-osx-for-webdev.sh | bash
+# $ curl -L https://raw.githubusercontent.com/ChristopherA/prepare-osx-for-webdev/master/prepare-osx.sh | bash
 
 # WARNING: Be careful about using `curl` piped `|` to bash or any other shell
 # as it can compromise your system. Only execute if you trust the source!
